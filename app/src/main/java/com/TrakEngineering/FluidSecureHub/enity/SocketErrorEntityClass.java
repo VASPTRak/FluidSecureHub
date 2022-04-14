1456ab723e606b4641b151fcf57dab303187772b
Prakash <prakash_shelar@vaspsolutions.com>
2022-04-14 10:21:52 +0530

    Initial Commit.

---
+++ app/src/main/java/com/TrakEngineering/FluidSecureHub/enity/SocketErrorEntityClass.java
@@ -0,0 +1,10 @@
+package com.TrakEngineering.FluidSecureHub.enity;
+
+public class SocketErrorEntityClass {
+
+    public String SiteId;
+    public String LogDateTime;
+    public String ErrorLogFileName;
+    public String TransactionId;
+
+}
